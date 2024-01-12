82832212807441cc746eac6b3363af2c707b1e37
hanbin <hnbn707@gmail.com>
2024-01-12 21:40:34 +0900

    Add : repository root - mongoDB connection 설정

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 go mod init
 go mod tidy
+
+## 사용하는 패키지
+
+```
+require (
+  github.com/ethereum/go-ethereum v1.13.5
+  github.com/naoina/toml v0.1.2-0.20170918210437-9fafd6967416
+  go.mongodb.org/mongo-driver v1.13.0
+)
+```
+
 go get github.com/naoina/toml
+go get go.mongodb.org/mongo-driver/mongo
--- repository/root.go
+++ repository/root.go
@@ -1,9 +1,22 @@
 package repository
 
-import "event/config"
+import (
+	"context"
+	"event/config"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
 
 type Repository struct{
-	config *config.Config
+	config 	*config.Config
+	
+	client  *mongo.Client
+	db 			*mongo.Database
+
+	Tx 						*mongo.Collection
+	NFT 					*mongo.Collection
+	NFTCollection *mongo.Collection
 }
 
 func NewRepository(config *config.Config) (*Repository, error) {
@@ -11,5 +24,21 @@ func NewRepository(config *config.Config) (*Repository, error) {
 		config: config,
 	}
 
+	var err error
+	ctx := context.Background()
+
+	mongoConf := config.Database.Mongo
+
+	if r.client, err = mongo.Connect(ctx, options.Client().ApplyURI(mongoConf.DataSource)); err != nil {
+		return nil, err
+	} else if err = r.client.Ping(ctx, nil); err != nil {
+		return nil, err
+	} else {
+		r.db = r.client.Database(mongoConf.DB)
+		r.Tx = r.db.Collection(mongoConf.Tx)
+		r.NFT = r.db.Collection(mongoConf.NFT)
+		r.NFTCollection = r.db.Collection(mongoConf.NFTCollection)
+
+	}
 	return &r, nil
 }
